888983e3009ccfde5b75e2dbe5549e66392d3792
Lionel RADISSON <makio135@gmail.com>
2024-01-15 12:36:41 +0100

    update script

--- main.js
+++ main.js
@@ -2,21 +2,9 @@ document.addEventListener('DOMContentLoaded', e => document.querySelectorAll('im
     this.style.display = 'none'
 }))
 
-document.querySelector('.container').innerHTML = hashes.map((hash,i) => {
-    let title = `${hash}`
-    
-    return {
-        html: `<div class="box"><img src="images/${hash}.png" title="${title}" loading="lazy"></div>`
-    }
-})
-// .sort((a, b) => a.paletteId - b.paletteId)
-.sort((a, b) => 1 - Math.random()*2)
-.map(d => d.html).join('')
-
-
-const imgs = document.querySelectorAll('.box')
-
-imgs.forEach(img => img.addEventListener('click', e => console.log(img.dataset)))
+document.querySelector('.container').innerHTML = hashes.map(hash => {
+    return `<div class="box"><img src="images/${hash}.png" title="${hash}" loading="lazy"></div>`
+}).sort((a, b) => 1 - Math.random()*2).join('')
 
 mediumZoom('img', {
     margin: 10,
