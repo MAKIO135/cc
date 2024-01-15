d22c3f155cf8b0987acd7fe3382da97e1bf6f002
Lionel RADISSON <makio135@gmail.com>
2024-01-15 12:48:17 +0100

    up

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
-document.addEventListener('DOMContentLoaded', e => document.querySelectorAll('img').forEach(img => img.onerror = function(){
-    this.style.display = 'none'
+document.addEventListener('DOMContentLoaded', e => document.querySelectorAll('img').forEach(img => {
+    img.onerror = function(){
+        this.style.display = 'none'
+    }
+    img.addEventListener('click', e => console.log(`https://24442d34-50d0-40e0-9906-3829c0e8760d-00-10er39lwhwzkv.janeway.replit.dev/#${img.title}`))
 }))
 
 document.querySelector('.container').innerHTML = hashes.map(hash => {
     return `<div class="box" ><img src="images/${hash}.png" title="${hash}" loading="lazy"></div>`
 }).sort((a, b) => 1 - Math.random()*2).join('')
 
-document.querySelectorAll('img').forEach(img => console.log(`https://24442d34-50d0-40e0-9906-3829c0e8760d-00-10er39lwhwzkv.janeway.replit.dev/#${img.title}`))
 
 mediumZoom('img', {
     margin: 10,
